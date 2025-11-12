addba5140aa2ad888f298eb570cd7f032a0719aa
이지민 <leechimins@naver.com>
2025-11-12 14:15:45 +0900

    ✨All: Week11
    feat: 일괄 업로드

---
+++ DataStructure/Week10/Week10/ctto.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int min20221590(int num) {
+    return &num;
+}
+
+int main() {
+    int num = 1111;
+    printf("%d", min20221590(num));
+    return 0;
+}
